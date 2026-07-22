b542e0d5d4b6ef4739b0ab7e5f2832c3c304b001
SumedhaKS <kssumedha24@gmail.com>
2026-07-22 20:02:55 +0530

    Fix: Stores documents metadata too

--- ai-service/app/ingest.py
+++ ai-service/app/ingest.py
@@ -20,11 +20,13 @@ def chunk_text(text, chunk_size=300, overlap=50):
 
     return chunks
 
-def store_chunks(chunks, embeddings):
-    ids = [f"chunk_{i}" for i in range(len(chunks))]
+def store_chunks(chunks, embeddings, document_id):
+    ids = [f"{document_id}_chunk_{i}" for i in range(len(chunks))]
+    metadatas = [{"document_id": document_id} for _ in chunks]
     collection.add(
         ids=ids,
         documents=chunks,
-        embeddings = embeddings.tolist()
+        embeddings = embeddings.tolist(),
+        metadatas = metadatas
     )
 
--- ai-service/app/retrieve.py
+++ ai-service/app/retrieve.py
@@ -1,10 +1,11 @@
 from .embed_utils import model
 from .db_utils import collection
 
-def retrieve_chunks(question, top_k=3):
+def retrieve_chunks(question, document_id, top_k=3):
     question_embedding = model.encode([question])
     results = collection.query(
         query_embeddings=question_embedding.tolist(),
-        n_results=top_k
+        n_results=top_k,
+        where = {"document_id": document_id}
     )
     return results["documents"][0]
