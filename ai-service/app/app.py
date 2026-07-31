bbfb48ef6362e7c975a51465cb7ba8e29a734e23
SumedhaKS <kssumedha24@gmail.com>
2026-07-31 19:59:25 +0530

    works

--- ai-service/app/app.py
+++ ai-service/app/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import os
+from pydantic import BaseModel
 
 from .ingest import extract_text, chunk_text, store_chunks
 from .embed_utils import embed_chunks
@@ -36,9 +37,14 @@ async def upload_document(document_id: str= Form(...) ,file: UploadFile = File(.
    os.remove(temp_path)
    return {"message": f"Stored {len(chunks)} chunks"}
 
+class AskRequest(BaseModel):
+    question: str
+    document_id: str
+
 @app.post("/ask")
-async def ask_question(question: str):
-    matches = retrieve_chunks(question)
-    answer = generate_answer(question, matches)
+async def ask_question(body: AskRequest):
+    print("Inside")
+    matches = retrieve_chunks(body.question, body.document_id)
+    answer = generate_answer(body.question, matches)
     return {"answer": answer}
 
