1d6db84af1b9329497417cb47cb04e9cffac92d0
SumedhaKS <kssumedha24@gmail.com>
2026-08-02 00:57:27 +0530

    added internal-api-key for security

--- ai-service/app/app.py
+++ ai-service/app/app.py
@@ -1,8 +1,10 @@
+from multiprocessing import process
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import os
 from pydantic import BaseModel
+from fastapi import HTTPException, Header, Depends
 
 from .ingest import extract_text, chunk_text, store_chunks
 from .embed_utils import embed_chunks
@@ -10,6 +12,9 @@ from .retrieve import retrieve_chunks
 from .generate import generate_answer
 
 app = FastAPI()
+
+PDF_MAGIC_BYTES = b"%PDF-"
+INTERNAL_API_KEY = os.getenv("INTERNAL_API_KEY")
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,20 +27,34 @@ app.add_middleware(
 def health_check():
     return {"message": "Good"}
 
+async def verify_internal_key(x_internal_key: str = Header(...)):
+    if x_internal_key != INTERNAL_API_KEY:
+        raise HTTPException(status_code = 403, detail= "Forbidden")
+
 @app.post("/upload")
-async def upload_document(document_id: str= Form(...) ,file: UploadFile = File(...)):
-   temp_path = f"temp_{file.filename}"
-   with open(temp_path, "wb") as buffer:
-    shutil.copyfileobj(file.file, buffer)
+async def upload_document(document_id: str= Form(...) ,file: UploadFile = File(...), _:None = Depends(verify_internal_key)):
+    if not file.filename.lower().endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Only PDF files are accepted")
 
-   text = extract_text(temp_path)
-   chunks = chunk_text(text)
-   embeddings = embed_chunks(chunks)
+    header = await file.read(5)
 
-   store_chunks(chunks, embeddings, document_id)
+    await file.seek(0)
 
-   os.remove(temp_path)
-   return {"message": f"Stored {len(chunks)} chunks"}
+    if header != PDF_MAGIC_BYTES:
+        raise HTTPException(status_code=400, detail="File is not valid PDF")
+
+    temp_path = f"temp_{file.filename}"
+    with open(temp_path, "wb") as buffer:
+     shutil.copyfileobj(file.file, buffer)
+
+    text = extract_text(temp_path)
+    chunks = chunk_text(text)
+    embeddings = embed_chunks(chunks)
+
+    store_chunks(chunks, embeddings, document_id)
+
+    os.remove(temp_path)
+    return {"message": f"Stored {len(chunks)} chunks"}
 
 class AskRequest(BaseModel):
     question: str
@@ -48,3 +67,4 @@ async def ask_question(body: AskRequest):
     answer = generate_answer(body.question, matches)
     return {"answer": answer}
 
+# todo: While deploying, deploy as a "Private service"
