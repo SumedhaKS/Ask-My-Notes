08a6ad787631ea2fb34125d68904bfaecbf433d0
SumedhaKS <kssumedha24@gmail.com>
2026-08-03 15:33:52 +0530

    works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='*' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/chat' element={<ChatApp />} />
